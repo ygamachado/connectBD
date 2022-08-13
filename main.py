b8371ed49ac7bc960d37f69119150dc423359872
ygamachado <72772753+ygamachado@users.noreply.github.com>
2022-08-13 19:14:58 -0300

    POO e MySQL funcionando!

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
-import create
 import usuario
 
+opcao=10
+def salvar():
+    nome = input("Digite o nome:")
+    email = input("Digite o email:")
+    user=usuario.User(1,nome,email)
+    save=input("Deseja savar s/n?")
+    if save=="s":
+        user.save_bd()
+def consultar(id):
+   ob=usuario.User.from_db(id)
+   print(ob.name,ob.email)
+def ler():
+   obs= usuario.User.lister()
+def alterar():
+    ler()
+    print("digite o indice:")
+    id=int(input())
+    ob = usuario.User.from_db(id)
+    print("Usuario selecioando: ",ob.name, ob.email)
+    name=input("digite novo nome:")
+    email=input("Digite novo email")
+    ob.update(id,name,email)
+def deletar():
+    ler()
+    id=int(input("Digite o indice do usuario a ser deletado: "))
+    ob = usuario.User.from_db(id)
+    print(ob.name, ob.email)
+    print("Tem certeza que deseja deletar o usuário: ",ob.name)
+    confirmar=input("s/n?")
+    if confirmar =="s":
+        ob.deletar(id)
+    else:
+        menu()
+def menu():
+    print("======================================\n"
+          "Menu\n"
+          "======================================\n"
+          "1-Cadastrar no Banco de Dados\n"
+          "2- Listar Todos os Usuários\n"
+          "3-Editar Usuário\n"
+          "4-Deletar Usuário\n"
+          "0-Sair\n"
+          "=======================================")
 
-def save():
-    print('Nome:')
-    nome = input()
-    print("email")
-    email = input()
-    user = usuario.User(1,nome,email)
-    return user
-    ob=create.Create()
+
+while (opcao != 0):
+    menu()
+    opcao = int(input("Digite a opção desejada:"))
+    if opcao == 1:
+        salvar()
+        print("salvar")
+    elif opcao == 2:
+        ler()
+    elif (opcao == 3):
+        alterar()
+    elif (opcao == 4):
+        deletar()
+    elif (opcao == 0):
+        break
--- usuario.py
+++ usuario.py
@@ -1,11 +1,7 @@
+import datetime
 import mysql.connector
 
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="teste"
-)
+
 
 class User:
     def __init__(self, ids=None, name=None, email=None):
@@ -13,9 +9,34 @@ class User:
         self.email = email
         self.name = name
 
-
+    def save_bd(self):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="teste"
+        )
+        cursor = connection.cursor()
+        sql = "INSERT INTO users (name, email, created) VALUES (%s, %s, %s)"
+        data = (
+            f'{self.name}',
+            f'{self.email}',
+            datetime.datetime.today()
+        )
+        cursor.execute(sql, data)
+        connection.commit()
+        userid = cursor.lastrowid
+        print("Foi cadastrado o novo usuário de ID:", userid)
+        cursor.close()
+        connection.close()
     @classmethod
     def from_db(cls,id):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="teste"
+        )
         query = f"SELECT id,name,email FROM teste.users where id = '{id}';"
         cursor = connection.cursor()
         cursor.execute(query)
@@ -26,3 +47,70 @@ class User:
         for r in row:
             lista.append(r)
         return User(lista[0],lista[1],lista[2])
+
+    def update(self,id,name,email):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="teste"
+        )
+        cursor = connection.cursor()
+
+        sql = "UPDATE users SET name = %s, email = %s WHERE id = %s"
+        data = (
+            f'{name}',
+            f'{email}',
+            f'{id}'
+        )
+
+        cursor.execute(sql, data)
+        connection.commit()
+
+        recordsaffected = cursor.rowcount
+
+        cursor.close()
+        connection.close()
+
+        print(recordsaffected, " registros alterados")
+    def deletar(self,id):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="teste"
+        )
+        cursor = connection.cursor()
+
+        sql = "DELETE FROM users WHERE id = %s"
+        data = (id,)
+
+        cursor.execute(sql, data)
+        connection.commit()
+
+        recordsaffected = cursor.rowcount
+
+        cursor.close()
+        connection.close()
+
+        print(recordsaffected, " registros excluídos")
+    @classmethod
+    def lister(cls):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="teste"
+        )
+        cursor = connection.cursor()
+
+        sqls = f"SELECT * FROM users"
+
+        cursor.execute(sqls)
+        results = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        for result in results:
+            print(result)
